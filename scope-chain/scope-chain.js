162586a12a21310d35b4f435a5a3c27be8274f9c
MatthewWeiChen <mattchen1021@gmail.com>
2020-11-23 23:34:15 -0800

    added files

---
+++ scope-chain/scope-chain.js
@@ -0,0 +1,30 @@
+function sayMyName() {
+  var a = 'a';
+  return function findName() {
+    var b = 'b';
+    console.log(c)
+    return function printName() {
+      var c = 'c';
+      return 'Andrei Neagoie'
+    }
+  }
+}
+
+sayMyName()()()
+
+function findName() {
+  var b = 'b';
+  return printName();
+}
+
+function printName() {
+  var c = 'c'
+  return 'Andrei Neagoie'
+}
+
+function sayMyName() {
+  var a = 'a';
+  return findName()
+}
+
+sayMyName()
